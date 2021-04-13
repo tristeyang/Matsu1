043f8d0598f59450b4738b62a4851f8a48c6df19
Triste <triste.yang@gmail.com>
2021-04-13 15:59:14 +0800

    Add files via upload

---
+++ mobile/javascript/bookmark_config.js
@@ -0,0 +1,1 @@
+﻿var ols=[{caption:"Cover",page:"1",url:"",level:"1",children:[]},{caption:"Introduction",page:"2",url:"",level:"1",children:[]},{caption:"Story",page:"4",url:"",level:"1",children:[]},{caption:"Rights",page:"32",url:"",level:"1",children:[]}];
